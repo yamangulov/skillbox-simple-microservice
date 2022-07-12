3217bff8e58b20e62161ef47f32d627a573e8cc0
YamangulovAN <yamangulov@gmail.com>
2022-07-12 14:49:45 +0300

    Задача "Поднять сервис на spring boot" готово, на проверку

---
+++ src/main/java/com/yamangulov/users/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.yamangulov.users.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello world from users service!";
+    }
+}
---
+++ src/test/java/com/yamangulov/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yamangulov.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
